14d72ed0bff84e4791393b474b9c0fdf0d2d1f83
MasonDoherty <masdoh2@gmail.com>
2023-07-16 12:16:25 -0700

    fixed resume page

--- src/scenes/Resume/Resume.jsx
+++ src/scenes/Resume/Resume.jsx
@@ -1,25 +1,24 @@
 import s from './Resume.module.scss';
 import { ReactComponent as FilesIcon } from '../../assets/resume-files.svg';
 import { ReactComponent as DownloadIcon } from '../../assets/download.svg';
-import { useEffect, useRef, useState } from 'react';
+import React, { useRef, useState, useEffect } from 'react';
 import BaseLayout from '../../layouts/BaseLayout/BaseLayout';
-import LinerProgress from '../../components/UIElements/LinerProgress/LinerProgress';
 import Button from '../../components/UIElements/Button/Button';
-//react-pdf
-import { Document, Page, pdfjs } from 'react-pdf';
-import 'react-pdf/dist/esm/Page/AnnotationLayer.css';
-pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
+import resume from '../../assets/Mason-Doherty-Software-Engineer.pdf';
+import { Page, Document, pdfjs } from 'react-pdf';
 
+pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 const resumeLink =
   'https://drive.google.com/file/d/1pinDOPriGODttNvXr16ZjUIu1IlBCBp5/view?usp=sharing';
-
 const Resume = () => {
   const pdfWrapper = useRef(null);
   const [pdfPageWidth, setPdfPageWidth] = useState(null);
+
   useEffect(() => {
     setPdfPageWidth(
       pdfWrapper.current?.getBoundingClientRect().width || null,
     );
+    removeTextLayerOffset();
   }, []);
 
   const removeTextLayerOffset = () => {
@@ -59,18 +58,8 @@ const Resume = () => {
         </Button>
 
         <div className={s.pdfWrapper} ref={pdfWrapper}>
-          <Document
-            loading={<LinerProgress />}
-            file={{
-              url: resumeLink,
-            }}
-          >
-            <Page
-              onLoadSuccess={removeTextLayerOffset}
-              loading={<LinerProgress />}
-              width={pdfPageWidth}
-              pageNumber={1}
-            />
+          <Document file={resume}>
+            <Page pageNumber={1} width={pdfPageWidth} />
           </Document>
         </div>
       </div>
